9f4e57747e512f4cab9aa65edc8e8e446571b9ea
Nikolay_WTF <kolan.gregorin01@gmail.com>
2024-11-11 09:10:41 +0300

    Changes made based on pull request comments

--- hw1.sql
+++ hw1.sql
@@ -1,6 +1,6 @@
 --Создаю таблицу courses
-create table courses(
-	id SERIAL primary key,
+CREATE TABLE courses(
+	id SERIAL PRIMARY KEY,
 	name VARCHAR(50),
 	is_exam BOOL,
 	min_grade INTEGER,
@@ -8,40 +8,40 @@ create table courses(
 );
 
 --Создаю таблицу groups
-create table groups(
-	id SERIAL primary key,
+CREATE TABLE groups(
+	id SERIAL PRIMARY KEY,
 	full_name VARCHAR(200),
 	short_name VARCHAR(50),
 	students_ids INTEGER[]
 );
 
 --Создаю таблицу students
-create table students(
-	id SERIAL primary key,
+CREATE TABLE students(
+	id SERIAL PRIMARY KEY,
 	first_name VARCHAR(50),
 	last_name VARCHAR(50),
 	group_id INTEGER
 );
 
 --Создаю таблицу machine_learning_course
-create table machine_learning_course(
-	student_id INTEGER,
+CREATE TABLE machine_learning_course(
+	student_id INTEGER REFERENCES students(id),
 	grade INTEGER,
 	grade_str VARCHAR(1));
 
 --Заполняю таблицу courses
-insert into courses(name, is_exam, min_grade, max_grade) values
+INSERT INTO courses(name, is_exam, min_grade, max_grade) VALUES
 ('Машинное обучение', true, 0, 100),
 ('Компьютерное зрение', true, 1, 5),
 ('Базы данных', true, 0, 100);
 
 --Заполняю таблицу groups
-insert into groups(full_name, short_name, students_ids) values
+INSERT INTO groups(full_name, short_name, students_ids) VALUES
 ('Искусственный интеллект и наука о данных', 'ИИНоД', '{1, 2, 3}'),
 ('Прикладная математика и информатика', 'ПМИ', '{4, 5, 6}');
 
 --Заполняю таблицу students
-insert into students(first_name, last_name, group_id) values
+INSERT INTO students(first_name, last_name, group_id) VALUES
 ('Николай', 'Григорьев', 1),
 ('Давид', 'Асатуров', 1),
 ('Вероника', 'Циунчик', 1),
@@ -50,31 +50,31 @@ insert into students(first_name, last_name, group_id) values
 ('София', 'Вилкова', 2);
 
 --Заполняю таблицу machine_learning_course
-insert into machine_learning_course(student_id, grade, grade_str) values
+INSERT INTO machine_learning_course(student_id, grade, grade_str) VALUES
 (1, 86, 'B'),
 (2, 100, 'A'),
 (3, 72, 'C');
 
 --Вывод таблицы machine_learning_course при альтернативном оценивании grade_str
-select 
+SELECT
 	student_id, grade,
-	case
-		when grade >= 85 then 'A'
-		when grade >= 70 then 'B'
-		when grade >= 60 then 'C'
-		when grade >= 50 then 'D'
-		else 'F'
-	end as grade_str
-from machine_learning_course;
-	
+	CASE
+		WHEN grade >= 85 THEN 'A'
+		WHEN grade >= 70 THEN 'B'
+		WHEN grade >= 60 THEN 'C'
+		WHEN grade >= 50 THEN 'D'
+		ELSE 'F'
+	END AS grade_str
+FROM machine_learning_course LIMIT 5;
+
 --Вывод среднего бала по дисциплине Машинное обучение
-select AVG(grade) from machine_learning_course;
+SELECT AVG(grade) FROM machine_learning_course;
 
 --Вывод всех студентов ИИНОД
-select * from students where group_id=1;
+SELECT * FROM students WHERE group_id=1 LIMIT 5;
 
 --Вывод всех студентов, фамилия которых начинается на букву 'А'
-select * from students WHERE last_name like 'А%';
+SELECT * FROM students WHERE last_name LIKE 'А%' LIMIT 5;
 
 --Вывод сех студентов, у которых балл по Машинному обучению больше 80
-select * from machine_learning_course where grade >= 80;
+SELECT * FROM machine_learning_course WHERE grade >= 80 LIMIT 5;
